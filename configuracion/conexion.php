e792c918c44ad6c05a9174d7bd4f4a84b9c4f33d
GemaRJ <gema_rj@hotmail.com>
2026-05-10 11:02:03 +0200

    Adaptada conexion para entorno local y hosting

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -1,5 +1,17 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "playgo");
+
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    // Conexión local con XAMPP
+    $conn = mysqli_connect("localhost", "root", "", "playgo");
+} else {
+    // Conexión online con InfinityFree
+    $conn = mysqli_connect(
+        "sql207.infinityfree.com",
+        "if0_41876219",
+        "TU_PASSWORD",
+        "if0_41876219_playgo"
+    );
+}
 
 if (!$conn) {
     die("Error de conexion: " . mysqli_connect_error());
