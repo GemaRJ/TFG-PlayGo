d6ba0e4bf063591f3a0d27748050a9fb2bd5cfef
GemaRJ <gema_rj@hotmail.com>
2026-05-10 11:32:29 +0200

    Adaptado modo invitado para despliegue

--- invitado_logic.php
+++ invitado_logic.php
@@ -1,16 +1,20 @@
 <?php
 // UBICACIÓN: /playgo/invitado_logic.php
 
-session_start();
+require_once "configuracion/sesiones.php";
+
+// Limpiar sesión anterior
 session_unset();
 session_destroy();
+
 session_start();
 
-// Crear sesión falsa
+// Crear sesión falsa de invitado
 $_SESSION['id'] = 0;
 $_SESSION['nombre'] = 'Explorador Anónimo';
 $_SESSION['tipo_usuario'] = 'invitado';
 
 // Enviar al panel
-header("Location: panel.php");
+header("Location: " . rutaBase() . "/panel.php");
+
 exit();
